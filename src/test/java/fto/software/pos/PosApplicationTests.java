5c81ae7a2854b378c17504cc005da3e75e8c7d19
F.Toutounchi <f.tutunchi@gmail.com>
2023-01-15 16:29:12 +0100

    initializing the main branch

---
+++ src/test/java/fto/software/pos/PosApplicationTests.java
@@ -0,0 +1,13 @@
+package fto.software.pos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
